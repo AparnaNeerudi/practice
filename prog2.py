2f10e7e7d913834bcd58b529c8b4537fddc2cb54
unknown <aparnaneerudi2513@gmail.com>
2024-11-11 22:22:16 +0530

    committing changes

---
+++ prog2.py
@@ -0,0 +1,11 @@
+# Simple program to add two numbers
+
+# Input numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
